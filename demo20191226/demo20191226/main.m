7c4afff5b2bb0a8705a143e53ac755f0db50410a
wang-hash <wangjingru.wjr@bytedance.com>
2019-12-26 19:22:06 +0800

    初始化

---
+++ demo20191226/demo20191226/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  demo20191226
+//
+//  Created by wangjingru on 2019/12/26.
+//  Copyright © 2019 wangjingru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
